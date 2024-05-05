bc2c235811744c7984819223a2731a32e1c82d61
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-06 02:13:32 +0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		port: 4173, // Set the desired port number
+		host: "0.0.0.0", // Bind to all network interfaces
+	},
+});
