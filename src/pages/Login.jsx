bff38cf4d00d56e436a114b375e9077159980b7c
Pallab_roy <roy.pallabtushar2234@gmail.com>
2024-05-08 00:50:03 +0600

    seller login link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -55,50 +55,50 @@ const Login = () => {
 			)}
 			<Headers />
 			{/* Register */}
-			<div className='bg-slate-200 mt-4'>
-				<div className='w-full justify-center items-center p-10 sm:p-5'>
+			<div className='mt-4 bg-slate-200'>
+				<div className='items-center justify-center w-full p-10 sm:p-5'>
 					<div className='w-[500px] md:w-auto sm:w-full mx-auto bg-white rounded-md'>
 						<div className='p-8 sm:p-5'>
-							<h2 className=' text-center w-full text-xl text-slate-600 font-bold'>Login</h2>
+							<h2 className='w-full text-xl font-bold text-center text-slate-600'>Login</h2>
 							{/* Register Section */}
 							<div>
 								<form onSubmit={(e) => handleSubmit(e)} className='text-slate-600'>
 									<div className='flex flex-col gap-1 mb-3'>
 										<label htmlFor='email'>Email</label>
-										<input onChange={(e) => handleChange(e)} type='email' id='email' name='email' placeholder='Email' className='w-full px-3 py-2 border border-slate-200 outline-0 focus:border-indigo-500 rounded-md' />
+										<input onChange={(e) => handleChange(e)} type='email' id='email' name='email' placeholder='Email' className='w-full px-3 py-2 border rounded-md border-slate-200 outline-0 focus:border-indigo-500' />
 									</div>
 
 									<div className='flex flex-col gap-1 mb-4'>
 										<label htmlFor='password'>Password</label>
-										<input onChange={(e) => handleChange(e)} type='password' id='password' name='password' placeholder='Password' className='w-full px-3 py-2 border border-slate-200 outline-0 focus:border-indigo-500 rounded-md' />
+										<input onChange={(e) => handleChange(e)} type='password' id='password' name='password' placeholder='Password' className='w-full px-3 py-2 border rounded-md border-slate-200 outline-0 focus:border-indigo-500' />
 									</div>
 
-									<button className='px-8 w-full py-2 bg-purple-500 shadow-lg hover:shadow-purple-500/30 text-white rounded-md'>Login</button>
+									<button className='w-full px-8 py-2 text-white bg-purple-500 rounded-md shadow-lg hover:shadow-purple-500/30'>Login</button>
 								</form>
-								<div className='flex justify-center items-center py-2'>
+								<div className='flex items-center justify-center py-2'>
 									<div className='h-[1px] bg-slate-300 w-[95%]'></div>
 									<span className='px-3 text-slate-600'>or</span>
 									<div className='h-[1px] bg-slate-300 w-[95%]'></div>
 								</div>
 
 								{/* Login with facebook */}
-								<button className='px-8 w-full py-2 bg-indigo-500 shadow-lg hover:shadow-indigo-500/30 text-white rounded-md flex justify-center items-center gap-2 mb-4'>
+								<button className='flex items-center justify-center w-full gap-2 px-8 py-2 mb-4 text-white bg-indigo-500 rounded-md shadow-lg hover:shadow-indigo-500/30'>
 									<span>
 										<FaFacebookF />
 									</span>
-									<span className='capitalize text-base sm:text-sm  whitespace-nowrap text-ellipsis overflow-hidden'>Login With Facebook</span>
+									<span className='overflow-hidden text-base capitalize sm:text-sm whitespace-nowrap text-ellipsis'>Login With Facebook</span>
 								</button>
 								{/* Login with google */}
-								<button className='px-8 w-full py-2  bg-orange-500 shadow-lg hover:shadow-orange-500/30 text-white rounded-md flex justify-center items-center gap-2 mb-4'>
+								<button className='flex items-center justify-center w-full gap-2 px-8 py-2 mb-4 text-white bg-orange-500 rounded-md shadow-lg hover:shadow-orange-500/30'>
 									<span>
 										<AiOutlineGooglePlus />
 									</span>
-									<span className='capitalize text-base sm:text-sm whitespace-nowrap text-ellipsis overflow-hidden'>Login With Google</span>
+									<span className='overflow-hidden text-base capitalize sm:text-sm whitespace-nowrap text-ellipsis'>Login With Google</span>
 								</button>
 							</div>
 
 							{/*  Already Account redirect to user login page   */}
-							<div className='text-center text-slate-600 pt-1'>
+							<div className='pt-1 text-center text-slate-600'>
 								<p>
 									You don't have an account?{" "}
 									<Link to={"/register"} className='text-blue-500'>
@@ -106,7 +106,7 @@ const Login = () => {
 									</Link>
 								</p>
 								<p>
-									<a className='text-blue-500 pr-1' target='_blank' href='http://localhost:5173/login'>
+									<a className='pr-1 text-blue-500' target='_blank' href='https://mern-ecommerce-dashboard-v1.onrender.com/login'>
 										Login
 									</a>
 									Seller Account
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -56,55 +56,55 @@ const Register = () => {
 			)}
 			<Headers />
 			{/* Register */}
-			<div className='bg-slate-200 mt-4'>
-				<div className='w-full justify-center items-center p-10 sm:p-5'>
+			<div className='mt-4 bg-slate-200'>
+				<div className='items-center justify-center w-full p-10 sm:p-5'>
 					<div className='w-[500px] md:w-auto sm:w-full mx-auto bg-white rounded-md'>
 						<div className='p-8 sm:p-5'>
-							<h2 className=' text-center w-full text-xl text-slate-600 font-bold'>Register</h2>
+							<h2 className='w-full text-xl font-bold text-center text-slate-600'>Register</h2>
 							{/* Register Section */}
 							<div>
 								<form onSubmit={(e) => handleSubmit(e)} className='text-slate-600'>
 									<div className='flex flex-col gap-1 mb-2'>
 										<label htmlFor='name'>Name</label>
-										<input onChange={(e) => handleChange(e)} required type='text' id='name' name='name' placeholder='Name' className='w-full px-3 py-2 border border-slate-200 outline-0 focus:border-indigo-500 rounded-md' />
+										<input onChange={(e) => handleChange(e)} required type='text' id='name' name='name' placeholder='Name' className='w-full px-3 py-2 border rounded-md border-slate-200 outline-0 focus:border-indigo-500' />
 									</div>
 
 									<div className='flex flex-col gap-1 mb-3'>
 										<label htmlFor='email'>Email</label>
-										<input onChange={(e) => handleChange(e)} required type='email' id='email' name='email' placeholder='Email' className='w-full px-3 py-2 border border-slate-200 outline-0 focus:border-indigo-500 rounded-md' />
+										<input onChange={(e) => handleChange(e)} required type='email' id='email' name='email' placeholder='Email' className='w-full px-3 py-2 border rounded-md border-slate-200 outline-0 focus:border-indigo-500' />
 									</div>
 
 									<div className='flex flex-col gap-1 mb-4'>
 										<label htmlFor='password'>Password</label>
-										<input onChange={(e) => handleChange(e)} required type='password' id='password' name='password' placeholder='Password' className='w-full px-3 py-2 border border-slate-200 outline-0 focus:border-indigo-500 rounded-md' />
+										<input onChange={(e) => handleChange(e)} required type='password' id='password' name='password' placeholder='Password' className='w-full px-3 py-2 border rounded-md border-slate-200 outline-0 focus:border-indigo-500' />
 									</div>
 
-									<button className='px-8 w-full py-2 bg-purple-500 shadow-lg hover:shadow-purple-500/30 text-white rounded-md'>Register</button>
+									<button className='w-full px-8 py-2 text-white bg-purple-500 rounded-md shadow-lg hover:shadow-purple-500/30'>Register</button>
 								</form>
-								<div className='flex justify-center items-center py-2'>
+								<div className='flex items-center justify-center py-2'>
 									<div className='h-[1px] bg-slate-300 w-[95%]'></div>
 									<span className='px-3 text-slate-600'>or</span>
 									<div className='h-[1px] bg-slate-300 w-[95%]'></div>
 								</div>
 
 								{/* Login with facebook */}
-								<button className='px-8 w-full py-2 bg-indigo-500 shadow-lg hover:shadow-indigo-500/30 text-white rounded-md flex justify-center items-center gap-2 mb-4'>
+								<button className='flex items-center justify-center w-full gap-2 px-8 py-2 mb-4 text-white bg-indigo-500 rounded-md shadow-lg hover:shadow-indigo-500/30'>
 									<span>
 										<FaFacebookF />
 									</span>
-									<span className='capitalize text-base sm:text-sm  whitespace-nowrap text-ellipsis overflow-hidden'>Login With Facebook</span>
+									<span className='overflow-hidden text-base capitalize sm:text-sm whitespace-nowrap text-ellipsis'>Login With Facebook</span>
 								</button>
 								{/* Login with google */}
-								<button className='px-8 w-full py-2  bg-orange-500 shadow-lg hover:shadow-orange-500/30 text-white rounded-md flex justify-center items-center gap-2 mb-4'>
+								<button className='flex items-center justify-center w-full gap-2 px-8 py-2 mb-4 text-white bg-orange-500 rounded-md shadow-lg hover:shadow-orange-500/30'>
 									<span>
 										<AiOutlineGooglePlus />
 									</span>
-									<span className='capitalize text-base sm:text-sm whitespace-nowrap text-ellipsis overflow-hidden'>Login With Google</span>
+									<span className='overflow-hidden text-base capitalize sm:text-sm whitespace-nowrap text-ellipsis'>Login With Google</span>
 								</button>
 							</div>
 
 							{/*  Already Account redirect to user login page   */}
-							<div className='text-center text-slate-600 pt-1'>
+							<div className='pt-1 text-center text-slate-600'>
 								<p>
 									You have already an account?{" "}
 									<Link to={"/login"} className='text-blue-500'>
@@ -112,7 +112,7 @@ const Register = () => {
 									</Link>
 								</p>
 								<p>
-									<a className='text-blue-500 pr-1' target='_blank' href='http://localhost:5173/login'>
+									<a className='pr-1 text-blue-500' target='_blank' href='https://mern-ecommerce-dashboard-v1.onrender.com/login'>
 										Login
 									</a>
 									Seller Account
